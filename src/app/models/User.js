9ef11dacaa9e32ae5508a9e4195e6393797da75c
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-06-13 01:27:41 -0300

    User model adjustment

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,33 +3,19 @@ const bcrypt = require('bcryptjs')
 const mongoose = require('../../database')
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    lowercase: true
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  passwordResetToken: {
-    type: String,
-    select: false
-  },
-  passwordResetExpires: {
-    type: Date,
-    select: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  name: { type: String, require: true },
+  email: { type: String, unique: true, required: true, lowercase: true },
+  password: { type: String, required: true, select: false },
+  passwordResetToken: { type: String, select: false },
+  passwordResetExpires: { type: Date, select: false },
+  favoritePosts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+      default: 0
+    }
+  ],
+  createdAt: { type: Date, default: Date.now }
 })
 
 UserSchema.pre('save', async function(next) {
@@ -38,6 +24,4 @@ UserSchema.pre('save', async function(next) {
   next()
 })
 
-const User = mongoose.model('User', UserSchema)
-
-module.exports = User
+module.exports = mongoose.model('User', UserSchema)
